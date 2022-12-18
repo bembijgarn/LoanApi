ee9d7c0c9c24318024e92bf61f7c96dc68608c3b
bembijgarn <sadwdwa2145@gmail.com>
2022-12-19 00:00:01 +0400

    test

--- LoanApi/FinalProject/TestProject1/UnitTest1.cs
+++ LoanApi/FinalProject/TestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using FinalProject.DATA;
 using FinalProject.Domain;
+using FinalProject.Interfaces;
 using FinalProject.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,21 +12,49 @@ namespace TestProject1
 {
     public class UnitTest1
     {
-        private readonly UserContext _context;
+        private readonly Iadminservice _adminservic;
+        private  readonly UserContext _context;
         
-        public UnitTest1(UserContext context)
+        public UnitTest1(Iadminservice service,UserContext context)
         {
+            this._adminservic = service;
             this._context = context;
+
         }
 
 
         [Fact]
-        public void Test1()
+        public void Getall_Test1()
         {              
-            var actual = new Adminservice(_context).GettAll();
+            var actual = _adminservic.GettAll();
             Assert.IsType<OkObjectResult>(actual as OkObjectResult);
         }
+        [Fact]
+        public void RemoveUser_Test()
+        {
+            int id = 10;
+            var actual = _adminservic.Removeuser(id);
 
+            Assert.NotNull(actual);
+        }
+        [Fact]
+        public void checkloan_Test()
+        {
+            string mail = "aeazkmy@gmail.com";
+            var actual = _adminservic.ChekLoan(mail);
+
+            Assert.NotNull(actual);
+        }
+        [Fact]
+        public void Blokcorunblokcuser_Test()
+        {
+            int id = 43;
+            string blockorunblokc = "BLOCK";
+            var actual = _adminservic.BlockOrUnblockUserbyId(id,blockorunblokc);
+            Assert.NotNull(actual);
+
+            
+        }
        
     }
 }
